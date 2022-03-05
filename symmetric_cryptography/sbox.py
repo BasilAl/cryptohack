43dea9b1f851f41f48f66a4d0d7a6f0ea5c95a2d
Bill Aliftiras <aliftiras.v@gmail.com>
2022-03-05 20:43:22 +0200

    2 more

--- symmetric_cryptography/sbox.py
+++ symmetric_cryptography/sbox.py
@@ -1,19 +1,3 @@
-"""
-The first step of each AES round is SubBytes. 
-This involves taking each byte of the state matrix and substituting it for a different byte in a preset 16x16 lookup table. 
-The lookup table is called a "Substitution box" or "S-box" for short, and can be perplexing at first sight. Let's break it down.
-In 1945 American mathematician Claude Shannon published a groundbreaking paper on Information Theory. It identified "confusion" as an essential property of a secure cipher. "Confusion" means that the relationship between the ciphertext and the key should be as complex as possible. Given just a ciphertext, there should be no way to learn anything about the key.
-
-If a cipher has poor confusion, it is possible to express a relationship between ciphertext, key, and plaintext as a linear function. For instance, in a Caesar cipher, ciphertext = plaintext + key. That's an obvious relation, which is easy to reverse. More complicated linear transformations can be solved using techniques like Gaussian elimination. Even low-degree polynomials, e.g. an equation like x^4 + 51x^3 + x, can be solved efficiently using algebraic methods. However, the higher the degree of a polynomial, generally the harder it becomes to solve – it can only be approximated by a larger and larger amount of linear functions.
-
-The main purpose of the S-box is to transform the input in a way that is resistant to being approximated by linear functions. S-boxes are aiming for high non-linearity, and while AES's one is not perfect, it's pretty close. The fast lookup in an S-box is a shortcut for performing a very nonlinear function on the input bytes. This function involves taking the modular inverse in the Galois field 2**8 and then applying an affine transformation which has been tweaked for maximum confusion. The simplest way to express the function is through the following high-degree polynomial:
-
-
-To make the S-box, the function has been calculated on all input values from 0x00 to 0xff and the outputs put in the lookup table.
-
-Implement sub_bytes, send the state matrix through the inverse S-box and then convert it to bytes to get the flag. 
-"""
-
 s_box = (
     0x63, 0x7C, 0x77, 0x7B, 0xF2, 0x6B, 0x6F, 0xC5, 0x30, 0x01, 0x67, 0x2B, 0xFE, 0xD7, 0xAB, 0x76,
     0xCA, 0x82, 0xC9, 0x7D, 0xFA, 0x59, 0x47, 0xF0, 0xAD, 0xD4, 0xA2, 0xAF, 0x9C, 0xA4, 0x72, 0xC0,
@@ -52,8 +36,6 @@ inv_s_box = (
     0x17, 0x2B, 0x04, 0x7E, 0xBA, 0x77, 0xD6, 0x26, 0xE1, 0x69, 0x14, 0x63, 0x55, 0x21, 0x0C, 0x7D,
 )
 
-from matrix import matrix2bytes
-
 state = [
     [251, 64, 182, 81],
     [146, 168, 33, 80],
@@ -61,15 +43,10 @@ state = [
     [64, 80, 182, 255],
 ]
 
-def sub_bytes(s, sbox=s_box):
-    sub_state = []
-    for i in range(len(state)):
-        l = []
-        for j in range(len(state[i])):
-            l.append(sbox[state[i][j]])
-        sub_state.append(l)
-    return matrix2bytes(sub_state)
 
-if __name__ == "__main__":
-    print(sub_bytes(state, sbox=inv_s_box))
+def sub_bytes(s, sbox=s_box):
+    pass
+
+
+print(sub_bytes(state, sbox=inv_s_box))
 
